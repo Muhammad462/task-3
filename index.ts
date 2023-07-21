9bbba0a29f29cfaeaba26dc3db193a980eb4e789
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-22 01:04:00 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,4 @@
+// Task 3: Print the name and qoute of auther
+var qoute = `\"I never teach my pupils i only provide the conditions in which they could learn.\"`;
+var author = `Albert Einstein`
+console. log(author,`once said`,qoute);
